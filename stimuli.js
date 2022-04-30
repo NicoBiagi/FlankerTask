ec35fffd0810ce3bbf33602ce5c34b8bf8832d4c
nico <nico.biagi@gmail.com>
2022-04-30 06:08:10 +0100

    stable working version

--- stimuli.js
+++ stimuli.js
@@ -1,280 +1,280 @@
 var test_stimuli = [
   {
     "block": "neutral",
-    "stimulus": "img/incong_ArrowLeft.png",
+    "stimulus": "img/incong_left.png",
     "correct_response": "ArrowLeft",
     "random": 0.013735742,
     "": 0
   },
   {
     "block": "neutral",
-    "stimulus": "img/cong_ArrowLeft.png",
+    "stimulus": "img/cong_left.png",
     "correct_response": "ArrowLeft",
     "random": 0.040234372,
     "": 0
   },
   {
     "block": "neutral",
-    "stimulus": "img/incong_ArrowLeft.png",
+    "stimulus": "img/incong_left.png",
     "correct_response": "ArrowLeft",
     "random": 0.078524044,
     "": 0
   },
   {
     "block": "neutral",
-    "stimulus": "img/cong_ArrowLeft.png",
+    "stimulus": "img/cong_left.png",
     "correct_response": "ArrowLeft",
     "random": 0.12856489,
     "": 0
   },
   {
     "block": "neutral",
-    "stimulus": "img/cong_ArrowRight.png",
+    "stimulus": "img/cong_right.png",
     "correct_response": "ArrowRight",
     "random": 0.13428139,
     "": 0
   },
   {
     "block": "neutral",
-    "stimulus": "img/incong_ArrowRight.png",
+    "stimulus": "img/incong_right.png",
     "correct_response": "ArrowRight",
     "random": 0.18691004,
     "": 0
   },
   {
     "block": "neutral",
-    "stimulus": "img/cong_ArrowRight.png",
+    "stimulus": "img/cong_right.png",
     "correct_response": "ArrowRight",
     "random": 0.203211476,
     "": 0
   },
   {
     "block": "neutral",
-    "stimulus": "img/incong_ArrowLeft.png",
+    "stimulus": "img/incong_left.png",
     "correct_response": "ArrowLeft",
     "random": 0.212705631,
     "": 0
   },
   {
     "block": "neutral",
-    "stimulus": "img/incong_ArrowRight.png",
+    "stimulus": "img/incong_right.png",
     "correct_response": "ArrowRight",
     "random": 0.244894815,
     "": 0
   },
   {
     "block": "neutral",
-    "stimulus": "img/cong_ArrowRight.png",
+    "stimulus": "img/cong_right.png",
     "correct_response": "ArrowRight",
     "random": 0.246830619,
     "": 0
   },
   {
     "block": "neutral",
-    "stimulus": "img/incong_ArrowRight.png",
+    "stimulus": "img/incong_right.png",
     "correct_response": "ArrowRight",
     "random": 0.27777124,
     "": 0
   },
   {
     "block": "neutral",
-    "stimulus": "img/cong_ArrowLeft.png",
+    "stimulus": "img/cong_left.png",
     "correct_response": "ArrowLeft",
     "random": 0.279889845,
     "": 0
   },
   {
     "block": "neutral",
-    "stimulus": "img/cong_ArrowLeft.png",
+    "stimulus": "img/cong_left.png",
     "correct_response": "ArrowLeft",
     "random": 0.309943564,
     "": 1
   },
   {
     "block": "neutral",
-    "stimulus": "img/cong_ArrowLeft.png",
+    "stimulus": "img/cong_left.png",
     "correct_response": "ArrowLeft",
     "random": 0.310220779,
     "": 2
   },
   {
     "block": "neutral",
-    "stimulus": "img/incong_ArrowLeft.png",
+    "stimulus": "img/incong_left.png",
     "correct_response": "ArrowLeft",
     "random": 0.367633463,
     "": 0
   },
   {
     "block": "neutral",
-    "stimulus": "img/incong_ArrowLeft.png",
+    "stimulus": "img/incong_left.png",
     "correct_response": "ArrowLeft",
     "random": 0.441379462,
     "": 1
   },
   {
     "block": "neutral",
-    "stimulus": "img/incong_ArrowRight.png",
+    "stimulus": "img/incong_right.png",
     "correct_response": "ArrowRight",
     "random": 0.462596395,
     "": 0
   },
   {
     "block": "neutral",
-    "stimulus": "img/cong_ArrowRight.png",
+    "stimulus": "img/cong_right.png",
     "correct_response": "ArrowRight",
     "random": 0.496187121,
     "": 0
   },
   {
     "block": "neutral",
-    "stimulus": "img/incong_ArrowRight.png",
+    "stimulus": "img/incong_right.png",
     "correct_response": "ArrowRight",
     "random": 0.498800562,
     "": 0
   },
   {
     "block": "neutral",
-    "stimulus": "img/incong_ArrowRight.png",
+    "stimulus": "img/incong_right.png",
     "correct_response": "ArrowRight",
     "random": 0.505984425,
     "": 1
   },
   {
     "block": "neutral",
-    "stimulus": "img/cong_ArrowLeft.png",
+    "stimulus": "img/cong_left.png",
     "correct_response": "ArrowLeft",
     "random": 0.515820008,
     "": 0
   },
   {
     "block": "neutral",
-    "stimulus": "img/cong_ArrowRight.png",
+    "stimulus": "img/cong_right.png",
     "correct_response": "ArrowRight",
     "random": 0.556167183,
     "": 0
   },
   {
     "block": "neutral",
-    "stimulus": "img/cong_ArrowLeft.png",
+    "stimulus": "img/cong_left.png",
     "correct_response": "ArrowLeft",
     "random": 0.609323761,
     "": 0
   },
   {
     "block": "neutral",
-    "stimulus": "img/cong_ArrowLeft.png",
+    "stimulus": "img/cong_left.png",
     "correct_response": "ArrowLeft",
     "random": 0.610814724,
     "": 1
   },
   {
     "block": "neutral",
-    "stimulus": "img/incong_ArrowLeft.png",
+    "stimulus": "img/incong_left.png",
     "correct_response": "ArrowLeft",
     "random": 0.616316745,
     "": 0
   },
   {
     "block": "neutral",
-    "stimulus": "img/incong_ArrowRight.png",
+    "stimulus": "img/incong_right.png",
     "correct_response": "ArrowRight",
     "random": 0.630493533,
     "": 0
   },
   {
     "block": "neutral",
-    "stimulus": "img/incong_ArrowRight.png",
+    "stimulus": "img/incong_right.png",
     "correct_response": "ArrowRight",
     "random": 0.631327636,
     "": 1
   },
   {
     "block": "neutral",
-    "stimulus": "img/cong_ArrowLeft.png",
+    "stimulus": "img/cong_left.png",
     "correct_response": "ArrowLeft",
     "random": 0.663833371,
     "": 0
   },
   {
     "block": "neutral",
-    "stimulus": "img/incong_ArrowLeft.png",
+    "stimulus": "img/incong_left.png",
     "correct_response": "ArrowLeft",
     "random": 0.665212597,
     "": 0
   },
   {
     "block": "neutral",
-    "stimulus": "img/incong_ArrowLeft.png",
+    "stimulus": "img/incong_left.png",
     "correct_response": "ArrowLeft",
     "random": 0.705920636,
     "": 1
   },
   {
     "block": "neutral",
-    "stimulus": "img/cong_ArrowLeft.png",
+    "stimulus": "img/cong_left.png",
     "correct_response": "ArrowLeft",
     "random": 0.720898234,
     "": 0
   },
   {
     "block": "neutral",
-    "stimulus": "img/cong_ArrowRight.png",
+    "stimulus": "img/cong_right.png",
     "correct_response": "ArrowRight",
     "random": 0.729204413,
     "": 0
   },
   {
     "block": "neutral",
-    "stimulus": "img/cong_ArrowRight.png",
+    "stimulus": "img/cong_right.png",
     "correct_response": "ArrowRight",
     "random": 0.733338048,
     "": 1
   },
   {
     "block": "neutral",
-    "stimulus": "img/incong_ArrowLeft.png",
+    "stimulus": "img/incong_left.png",
     "correct_response": "ArrowLeft",
     "random": 0.747057707,
     "": 0
   },
   {
     "block": "neutral",
-    "stimulus": "img/incong_ArrowRight.png",
+    "stimulus": "img/incong_right.png",
     "correct_response": "ArrowRight",
     "random": 0.775165996,
     "": 0
   },
   {
     "block": "neutral",
-    "stimulus": "img/incong_ArrowRight.png",
+    "stimulus": "img/incong_right.png",
     "correct_response": "ArrowRight",
     "random": 0.8104268,
     "": 1
   },
   {
     "block": "neutral",
-    "stimulus": "img/cong_ArrowRight.png",
+    "stimulus": "img/cong_right.png",
     "correct_response": "ArrowRight",
     "random": 0.853049056,
     "": 0
   },
   {
     "block": "neutral",
-    "stimulus": "img/incong_ArrowLeft.png",
+    "stimulus": "img/incong_left.png",
     "correct_response": "ArrowLeft",
     "random": 0.905430223,
     "": 0
   },
   {
     "block": "neutral",
-    "stimulus": "img/cong_ArrowRight.png",
+    "stimulus": "img/cong_right.png",
     "correct_response": "ArrowRight",
     "random": 0.957619001,
     "": 0
   },
   {
     "block": "neutral",
-    "stimulus": "img/cong_ArrowRight.png",
+    "stimulus": "img/cong_right.png",
     "correct_response": "ArrowRight",
     "random": 0.99000205,
     "": 1
